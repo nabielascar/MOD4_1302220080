14a70857666fc83962e17fd37e260c58963cc200
nabielascar <ascarnabiel@gmail.com>
2024-03-16 09:01:22 +0700

    menambahkan class kodeBuah

--- MOD4_1302220080/Program.cs
+++ MOD4_1302220080/Program.cs
@@ -1,10 +1,22 @@
 ﻿namespace MOD4_1302220080
 {
-    internal class Program
+    public class Program
     {
+        public enum namaBuah
+        {apel, aprikot, alpukat, pisang, paprika, blackberry, ceri, kelapa, jagung, kurma, durian, anggur, melon, semangka}
+
+        public static string getKodeBuah(namaBuah namaBuah)
+        {
+            string[] kodeBuah = { "A00, B00, C00, D00, E00, F00, H00, I00, J00, K00, L00, M00, N00, O00" };
+            return kodeBuah[(int)namaBuah];            
+        }
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            namaBuah namaBuah = new namaBuah();
+
+            Console.WriteLine("Nama buah: " + namaBuah + kodeBuah);
         }
+
     }
 }
--- MOD4_1302220080/obj/Debug/net8.0/MOD4_1302220080.AssemblyInfo.cs
+++ MOD4_1302220080/obj/Debug/net8.0/MOD4_1302220080.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MOD4_1302220080")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed3dcf957ad469de564ff98f5ab4aacd3df5e3d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("MOD4_1302220080")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MOD4_1302220080")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
