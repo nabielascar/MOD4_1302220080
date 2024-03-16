ed3dcf957ad469de564ff98f5ab4aacd3df5e3d8
nabielascar <ascarnabiel@gmail.com>
2024-03-16 08:03:48 +0700

    first commit

---
+++ MOD4_1302220080/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MOD4_1302220080
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
